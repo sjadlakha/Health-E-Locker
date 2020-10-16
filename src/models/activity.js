75373b34ac127d62793b378cfa4a2aafd8d0b79f
sjadlakha <sjadlakha@gmail.com>
2020-10-16 10:41:21 +0530

    Fixed activity model

--- src/models/activity.js
+++ src/models/activity.js
@@ -10,7 +10,7 @@ const activitySchema=new mongoose.Schema({
         required: true
     },
     startDate:{
-        type: Date,
+        type: String,
         required:true
     },
     startTime: {
